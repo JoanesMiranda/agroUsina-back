baecdf475e33310af16199140bf9f8b9b934d665
Joanes Souto <joanes.souto@gmail.com>
2020-09-14 02:08:07 -0300

    update env

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
+require('dotenv/config');
+
 module.exports = {
     dialect: process.env.DIALECT,
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
