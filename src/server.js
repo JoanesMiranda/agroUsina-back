853c3096a1e00ff629b1f00ae504bda02532d983
Joanes Souto <joanes.souto@gmail.com>
2020-09-15 11:29:52 -0300

    update cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
+const cors = require('cors');
 const corsConfig = require('./config/cors');
+
 const routes = require('./routes');
 
 require('dotenv/config');
